caf7fc9d746c32753521278c13436565c23aa6a0
xiaoyureed <xiaoyureed@users.noreply.github.com>
2022-02-23 08:50:00 +0000

    deploy: 6ab98e07f9ab7b54b70143b1bcfe2ad6eb7bea39

---
+++ _next/static/z-h3R1HL2wS8fy5s6nlQ-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/84a0a28d77eb43eb.css","static/chunks/pages/index-f56961917e2a0425.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
